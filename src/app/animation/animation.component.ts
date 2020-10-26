d6cc0d383a6d999cc53e2b9aad85670fc037f3ef
miya_xu <miyaxu0906@gmail.com>
2020-10-26 22:44:57 +0800

    tween

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -1,46 +1,18 @@
-declare global {
-  interface Window { 
-    mozRequestAnimationFrame?: any;
-    cancelRequestAnimFrame?: any;
-    webkitCancelRequestAnimationFrame?: any;
-    mozCancelRequestAnimationFrame?: any;
-    oCancelRequestAnimationFrame?: any;
-    msCancelRequestAnimationFrame?: any;
-  }
-}
-
-window.requestAnimationFrame = (function(){
-  return  window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function(callback: any ){
-    window.setTimeout(callback, 1000 / 60);
-  };
-})();
-
-window.cancelAnimationFrame = (function() {
-  return window.cancelAnimationFrame ||
-  window.webkitCancelRequestAnimationFrame ||
-  window.mozCancelRequestAnimationFrame ||
-  window.oCancelRequestAnimationFrame ||
-  window.msCancelRequestAnimationFrame ||
-  clearTimeout;
-})();
-
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { interval, Observable, of, timer } from 'rxjs';
+import { concatAll, finalize, takeUntil } from 'rxjs/operators';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-animation',
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.sass']
 })
-export class AnimationComponent implements OnInit, OnDestroy {
+export class AnimationComponent implements OnInit {
 
   @ViewChild('canvas', { static: true }) 
   canvas: ElementRef<HTMLCanvasElement>;
   ctx: CanvasRenderingContext2D;
-
-  requestID: number;
 
   cpx = 550;
   cpy = 300;
@@ -51,18 +23,75 @@ export class AnimationComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.loop(0);
+
+    // this.start();
+
+    // console.log(Math.floor(new Date().getTime() / 1000));
+    // interval(1000)
+    // .pipe(
+    //   takeUntil(timer(3000)),
+    //   finalize(() => {
+    //     console.log('done');
+    //   })
+    // ).subscribe((n) => {
+    //   console.log(n, Math.floor(new Date().getTime() / 1000));
+    // });
+
+    var data = {
+      n: 0,
+    };
+    
+    anime({
+      targets: data,
+      n: 100,
+      easing: 'linear',
+      duration: 9000,
+      update: () => {
+        this.draw();
+        this.counter = 550 * (data.n / 100);
+      }
+    });
   }
 
-  ngOnDestroy() {
-    cancelAnimationFrame(this.requestID);
+  start() {
+    // console.log(Math.floor(new Date().getTime() / 1000));
+    const timeine = of(this.first(), this.second(), this.third());
+    const state = timeine.pipe(concatAll());
+    state.subscribe(val => console.log(val));
+  }
+
+  first() {
+    return new Observable((observer) => {
+      setTimeout(() => {
+        console.log('first', Math.floor(new Date().getTime() / 1000));
+        observer.complete();
+      }, 1000);
+    });
+  }
+
+  second() {
+    return new Observable((observer) => {
+      setTimeout(() => {
+        console.log('second', Math.floor(new Date().getTime() / 1000));
+        observer.complete();
+      }, 1000);
+    });
+  }
+
+  third() {
+    return new Observable((observer) => {
+      setTimeout(() => {
+        console.log('third', Math.floor(new Date().getTime() / 1000));
+        observer.complete();
+      }, 1000);
+    });
   }
 
   clearRect() {
     this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
-  draw(currentTime: number) {
+  draw() {
     this.clearRect();
 
     this.ctx.save();
@@ -72,25 +101,13 @@ export class AnimationComponent implements OnInit, OnDestroy {
     this.ctx.stroke();
  
     this.ctx.globalCompositeOperation = 'destination-in';
-    if (this.counter >= 550) {
-      this.counter = 0;
-    }
     this.ctx.fillRect(50, 20, this.counter += 1, 280);
     this.ctx.restore();
-
-    this.counter += 1;
-
 
     this.ctx.save();
     this.ctx.beginPath();
     this.ctx.arc(this.cpx, this.cpy, 5, 0, 2 * Math.PI);
     this.ctx.stroke();
     this.ctx.restore();
-  }
-
-  loop(currentTime: number) {
-    // console.log('loop', this.requestID);
-    this.draw(currentTime);
-    this.requestID = requestAnimationFrame(this.loop.bind(this));
   }
 }
